e1d698ad3ce8afaeb36eac49d7fc1e205d3f62d0
dev choudhary <devchoudhary5365@gmail.com>
2024-05-27 18:06:09 +0530

    changes

--- server.js
+++ server.js
@@ -1,13 +1,72 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const passportConfig = require("./lib/passportConfig");
+// const cors = require("cors");
+// const fs = require("fs");
+
+// // MongoDB
+// mongoose
+//   .connect("mongodb://localhost:27017/jobPortal", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log("Connected to DB"))
+//   .catch((err) => console.log(err));
+
+// // Initializing directories
+// if (!fs.existsSync("./public")) {
+//   fs.mkdirSync("./public");
+// }
+// if (!fs.existsSync("./public/resume")) {
+//   fs.mkdirSync("./public/resume");
+// }
+// if (!fs.existsSync("./public/profile")) {
+//   fs.mkdirSync("./public/profile");
+// }
+
+// const app = express();
+// const port = 4444;
+
+// app.use(bodyParser.json()); // support JSON encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// // Setting up middlewares
+// app.use(cors());
+// app.use(express.json());
+// app.use(passportConfig.initialize());
+
+// // Routing
+// app.use("/auth", require("./routes/authRoutes"));
+// app.use("/api", require("./routes/apiRoutes"));
+// app.use("/host", require("./routes/downloadRoutes"));
+
+// // Dynamically import the upload routes
+// (async () => {
+//   try {
+//     const uploadRoutes = await import("./routes/uploadRoutes.js");
+//     app.use("/upload", uploadRoutes.default || uploadRoutes); // Handle default export
+//   } catch (err) {
+//     console.error("Error loading upload routes:", err);
+//   }
+
+//   app.listen(port, () => {
+//     console.log(`Server started on port ${port}!`);
+//   });
+// })();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passportConfig = require("./lib/passportConfig");
 const cors = require("cors");
 const fs = require("fs");
+require('dotenv').config();  // Load environment variables from .env file
 
 // MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/jobPortal", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -28,7 +87,7 @@ if (!fs.existsSync("./public/profile")) {
 }
 
 const app = express();
-const port = 4444;
+const port = process.env.PORT || 4444;
 
 app.use(bodyParser.json()); // support JSON encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -56,3 +115,4 @@ app.use("/host", require("./routes/downloadRoutes"));
     console.log(`Server started on port ${port}!`);
   });
 })();
+
